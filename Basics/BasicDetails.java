c4cf16c30a8af33fcf0bdf4ee56cf4f10f103ebd
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-09 14:40:56 +0530

    Basic details

--- Basics/BasicDetails.java
+++ Basics/BasicDetails.java
@@ -1,8 +1,9 @@
+
 public class BasicDetails{
     public static void main(String[] args) {
         System.out.println("My name is Sandhya.");
         System.out.println("coming from AndhraPradesh.");
         System.out.println("Pursuing Master of computer Applications in Aditya college of Engineering and Technology.");
 
-    }
+    }`
 }
