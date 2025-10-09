615a9801c303a4d38a54e14acfe76e0612a2ec94
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-09 14:41:44 +0530

    Variables

---
+++ Basics/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main(String[] args) {
+        byte b = 1;
+        short s = 323; 
+        int i = 2345689;
+        long l = 1234589790;
+        char c = 'S';
+        float f = 3.12f;
+        double d = 2.43322122;
+        System.out.println("The value of byte is : "+b);
+        System.out.println("The value of short is : "+s);
+        System.out.println("The value of int is : "+i);
+        System.out.println("The value of long is : "+l);
+        System.out.println("The value of char is : "+c);
+        System.out.println("The value of float is : "+f);
+        System.out.println("The value of double is : "+d);
+    }
+}
