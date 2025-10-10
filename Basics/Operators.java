d0c43f39796793bcfa19010f39ddddabdb2b3cba
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-10 14:15:38 +0530

    Operators

---
+++ Basics/Operators.java
@@ -0,0 +1,10 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a=10, b=20;
+        System.out.println(" sum : "+(a+b));
+        System.out.println("Difference : "+(a-b));
+        System.out.println("Multiplication : "+(a*b));
+        System.out.println("Division : "+(a/b));
+        System.out.println("Modulo division : "+(a%b));
+    }
+}
