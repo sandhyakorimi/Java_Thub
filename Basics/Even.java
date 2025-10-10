84074fec27c20777a292a871f84dca666796cc5e
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-10 14:26:45 +0530

    Even number check

--- Basics/Even.java
+++ Basics/Even.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Even {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number to check even or odd : ");
+        System.out.print("Enter a number to check even or odd : ");
         int val = sc.nextInt();
         if(val%2==0){
             System.out.println(val+" is Even.");
