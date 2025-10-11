350d0813ea2dfe578e40691ca4740ed14250e1cb
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-11 14:42:06 +0530

    Even or odd

--- Basics/Even.java
+++ Basics/Even.java
@@ -4,10 +4,13 @@ public class Even {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number to check even or odd : ");
         int val = sc.nextInt();
-        if(val%2==0){
-            System.out.println(val+" is Even.");
-        }
-        else System.out.println(val+" is Odd");
+        // if(val%2==0){
+        //     System.out.println(val+" is Even.");
+        // }
+        // else System.out.println(val+" is Odd");
+
+        String res = (val%2==0)?"Even":"Odd";
+        System.out.println("Entered num "+val+ " is: "+res);
 
         sc.close();
     }
