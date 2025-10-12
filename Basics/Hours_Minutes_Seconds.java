1a09aeaedc3812c90229dbf7ce5d0a7484d5f2a5
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-12 19:12:17 +0530

    Hours to minutes and seconds

---
+++ Basics/Hours_Minutes_Seconds.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Hours_Minutes_Seconds {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Hours : ");
+        int hrs = sc.nextInt();
+        int min = hrs * 60;
+        int sec = min *60;
+        System.out.println( hrs + " hours contains : "+min+ " minutes");
+        System.out.println(hrs+ " hours contains : "+sec+" seconds");
+        sc.close();
+    }
+}
