2daf92272f7c4232e7ccbb4626b0a24620b48b58
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-12 19:13:50 +0530

    Days_years_weeks_day

---
+++ Basics/Days_years_weeks_day.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Days_years_weeks_day {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n_days = sc.nextInt();
+        int years = n_days/365;
+        int weeks = (n_days%365)/7;
+        int days = (n_days%365)%7;
+        
+        sc.close();
+    }
+}
