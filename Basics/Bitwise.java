aa1138d919ad7e4a2a22859ec93eb51552c3f764
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-16 14:37:38 +0530

    Bistwise

---
+++ Basics/Bitwise.java
@@ -0,0 +1,11 @@
+public class Bitwise {
+    public static void main(String[] args){
+        int a = 20;
+        int b = 10;
+        System.out.println("a&b: "+(a&b));
+        System.out.println("a|b: "+(a|b));
+        System.out.println("a^b: "+(a^b));
+        System.out.println("b<<2: "+(b<<2));
+        System.out.println("b>>2: "+(b>>2));
+    }
+}
