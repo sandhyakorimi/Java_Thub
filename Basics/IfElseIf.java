65eb44dc518e4c1ea5bfe3a236333145d47a699e
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-17 12:07:56 +0530

    if else if

---
+++ Basics/IfElseIf.java
@@ -0,0 +1,11 @@
+public class IfElseIf {
+    public static void main(String[] args){
+        int a = 4;
+        if(a > 0){
+            System.out.println("Positive Number.");
+        }else if(a < 0){
+            System.out.println("Negative Number: ");
+        }else 
+        System.out.println("Zero.");
+    }
+}
