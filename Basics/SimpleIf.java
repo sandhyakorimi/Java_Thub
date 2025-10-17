73e51357ef00422d542af96aa05f03fe46d7a06d
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-17 12:09:57 +0530

    simple if

---
+++ Basics/SimpleIf.java
@@ -0,0 +1,8 @@
+public class SimpleIf {
+    public static void main(String[] args){
+        int a = 20;
+        if(a == 20){
+            System.out.println("a value is: "+a);
+        }
+    }
+}
