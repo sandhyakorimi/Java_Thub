0663b79b0a8e32f535826b05ac5cc80edf4ce322
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-23 15:07:18 +0530

    divisible by 5 and 11

---
+++ conditional/Divisible.java
@@ -0,0 +1,15 @@
+package conditional;
+
+import java.util.Scanner;
+
+public class Divisible {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner((System.in));
+        System.out.print("Enter a Number : ");
+        int num = sc.nextInt();
+        System.out.println();
+        String divisible = ((num%5==0)&&(num%11==0))?"Divisible":"Not divisible";
+        System.out.println(num + " is "+divisible+" by  5 and 11 !");
+        sc.close();
+    }
+}
