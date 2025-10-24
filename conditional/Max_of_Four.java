345c7b4db6625aec52b70918328464b995fddeb6
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-24 20:43:07 +0530

    Max_of_Four

---
+++ conditional/Max_of_Four.java
@@ -0,0 +1,15 @@
+package conditional;
+
+import java.util.Scanner;
+
+public class Max_of_Four {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner((System.in));
+        System.out.print("Enter first number : ");
+        int a = sc.nextInt(); System.out.println();
+        System.out.print("Enter secong Number : ");
+        int b = sc.nextInt(); System.out.println();
+        System.out.println("Enter third Number : ");
+        sc.close();
+    }
+}
