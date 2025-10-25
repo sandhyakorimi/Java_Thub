1b409e2f2ca06387e4227865efcad217ca7bd2e1
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-25 20:02:12 +0530

    check character

---
+++ conditional/Check_Character.java
@@ -0,0 +1,25 @@
+package conditional;
+
+import java.util.Scanner;
+
+public class Check_Character {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner((System.in));
+        System.out.print("Enter a character : ");
+        char ch=sc.next().charAt(0);
+        System.out.println();
+        if(ch>='A'&&ch<='Z'){
+            System.out.println(ch+" is Uppercase Letter !");
+        }
+        if(ch>='a'&&ch<='z'){
+            System.out.println(ch+" is Lowercase Letter !");
+        }
+        if(ch>='1'&&ch<='9'){
+            System.out.println(ch+" is a Number !");
+        }
+        else{
+            System.out.println(ch+" is special character !");
+        }
+        sc.close();
+    }
+}
