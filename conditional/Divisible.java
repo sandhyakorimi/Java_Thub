edf1d10b65b5020fb3e4e8f9f8639d294677a05a
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-26 20:05:37 +0530

    divisible

--- conditional/Divisible.java
+++ conditional/Divisible.java
@@ -10,6 +10,6 @@ public class Divisible {
         System.out.println();
         String divisible = ((num%5==0)&&(num%11==0))?"Divisible":"Not divisible";
         System.out.println(num + " is "+divisible+" by  5 and 11 !");
-        sc.close();
+        sc.close(); 
     }
 }   
