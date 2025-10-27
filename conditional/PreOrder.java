0993352d8230eb210f63ce3291211087686f2815
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-27 15:42:54 +0530

    Preorder

--- conditional/PreOrder.java
+++ conditional/PreOrder.java
@@ -21,12 +21,12 @@ public class PreOrder {
         preorder(root);
         return res; 
     }
-    
+
     private void preorder(TreeNode node) {
         if (node == null) {
             return;
         }
-        res.add(node.val);
+        res.add(node.val);  
         preorder(node.left);
         preorder(node.right);
     }
